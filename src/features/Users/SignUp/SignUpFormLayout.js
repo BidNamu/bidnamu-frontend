73726442ac0d7a601dd4c5e04c540dc7490e4c77
sanghyeonLee2 <hyeon012366@gmail.com>
2023-12-27 16:29:13 +0900

    design: styled-components 회원가입 폼 중앙배치, width, padding 설정하는 파일 생성

---
+++ src/features/Users/SignUp/SignUpFormLayout.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+export const SignUpFormLayout = styled.div`
+  display: flex;
+  width: 100%;
+  padding-top: 100px;
+  text-align: center;
+  justify-content: center;
+`
