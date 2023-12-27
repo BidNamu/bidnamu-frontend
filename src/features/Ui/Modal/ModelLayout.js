9724a2b0169fc7888ec8f247e02467eed4734937
sanghyeonLee2 <hyeon012366@gmail.com>
2023-12-27 16:37:06 +0900

    design: 로그인 모달창 styled-components 파일로 러프하게 스타일 작성

---
+++ src/features/Ui/Modal/ModelLayout.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+export const LogInLayout = styled.div`
+  span{
+    cursor: pointer;
+  }
+  position: absolute;
+  left: 50%; 
+  transform: translateX(-50%);
+  text-align: center;
+  border: 1px solid red;
+  background-color: white;
+`
