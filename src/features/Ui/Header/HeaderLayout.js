588444ef93fdd3660cfc45daaa50028e4f7e5373
sanghyeonLee2 <hyeon012366@gmail.com>
2023-12-27 16:07:49 +0900

    design: 헤더에 적용할 styled-components 파일 추가 후 absolute로 로고 이미지 좌즉배치, 회원가입 및 로그인 url이동하는 버튼 우측 배치, 마진 설정

---
+++ src/features/Ui/Header/HeaderLayout.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const UserAuthDiv = styled.div`
+  margin: 10px 200px 50px;
+  h3 {
+    float: right;
+    margin-left: 45px;
+    cursor: pointer;
+  }
+`
+export const LogoImg = styled.img`
+  margin-left: 200px;
+    position: absolute
+`
