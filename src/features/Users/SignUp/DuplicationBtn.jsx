ee7de44ecd85bea667917ceed541c085bed4064b
sanghyeonLee2 <hyeon012366@gmail.com>
2023-12-27 16:31:58 +0900

    feat: 닉네임, 비밀번호 중복확인 할 수 있는 버튼 컴포넌트 생성, 버튼 클릭 시 중복확인 종류에 따라 서버에 요청하는 기능 추가

---
+++ src/features/Users/SignUp/DuplicationBtn.jsx
@@ -0,0 +1,36 @@
+import React, {useRef} from 'react';
+import axios from "../../../apis/utils/instance"
+import {useQuery, useQueryClient} from "react-query"
+
+function DuplicationBtn({targetValue, type}) {
+    const duplicationCheck = async () => {
+        let endPoint = ""
+        if (type === "nickname") {
+            endPoint = `users/nickname/duplicated/${targetValue}`
+        }
+        if (type === "email") {
+            endPoint = `users/email/duplicated/${targetValue}`
+        }
+        try {
+            const result = await axios.get(endPoint)
+            if (result.status === 200) {
+                alert(`사용가능한 ${type}입니다.`)
+                return
+            }
+            if (result.status === 409) {
+                alert(`중복된 ${type} 입니다.`)
+            }
+        } catch (err) {
+            console.log(err.message)
+            alert("서버 통신 실패")
+        }
+    }
+
+    return (
+        <>
+            <button type={"button"} onClick={duplicationCheck}>중복확인</button>
+        </>
+    );
+}
+
+export default DuplicationBtn;
