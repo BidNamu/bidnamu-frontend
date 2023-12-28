5fecc74fd3dfc51c718e203fe9ee1aed98ff7577
sanghyeonLee2 <hyeon012366@gmail.com>
2023-12-28 22:25:53 +0900

    refactor: 회원가입 입력창 state 커스텀 훅으로 분리

--- src/features/Users/SignUp/SignUpForm.jsx
+++ src/features/Users/SignUp/SignUpForm.jsx
@@ -3,10 +3,11 @@ import {SignUpFormLayout} from "./SignUpFormLayout";
 import DuplicationBtn from "./DuplicationBtn";
 import axios from "../../../apis/utils/instance";
 import {useNavigate} from "react-router-dom";
+import {useInput} from "../../../hooks/useInput";
 
 function SignUpForm(props) { //닉네임(중복확인), 이메일(중복확인), 비밀번호
     const navigate = useNavigate()
-    const [signUpFormState, setSignUpFormState] = useState({
+    const [handleInputChg, inputFormState] = useInput({
         nickname: "",
         email: "",
         password: ""
@@ -17,13 +18,7 @@ function SignUpForm(props) { //닉네임(중복확인), 이메일(중복확인),
             emailDuplicationOk: false
         }
     )
-
     const checkPwRef = useRef(null)
-    const SignUpFormOnChg = (e) => {
-        setSignUpFormState((prev) => ({
-            ...prev, [e.target.name]: e.target.value
-        }))
-    }
     const formOnSubmit = async (e) => {
         e.preventDefault()
         if (!isDuplicationOk.emailDuplicationOk) {
@@ -32,25 +27,21 @@ function SignUpForm(props) { //닉네임(중복확인), 이메일(중복확인),
         if (!isDuplicationOk.nicknameDuplicationOk) {
             return alert("닉네임 중복확인을 하세요")
         }
-        const result = await axios.post("/users", signUpFormState)
+        const result = await axios.post("/users", inputFormState)
         try {
             console.log(result)
             if (result.status === 201) {
                 alert("회원가입을 완료하였습니다.")
                 return navigate("/")
             }
-            if (result.status === 409) {
-                return alert("중복된 정보가 있습니다")
-            }
         } catch (err) {
             console.log("실패")
-            alert("실패 하였습니다.")
+            alert("서버 통신 실패")
         }
-
     }
 
     const checkPwOnChg = (e) => {
-        if (signUpFormState.password === e.target.value) {
+        if (inputFormState.password === e.target.value) {
             checkPwRef.current.innerText = "비밀번호가 일치합니다."
             return
         }
@@ -63,18 +54,18 @@ function SignUpForm(props) { //닉네임(중복확인), 이메일(중복확인),
             <form onSubmit={formOnSubmit}>
                 <h2>회원가입</h2>
                 <p>닉네임</p>
-                <input type={"text"} name={"nickname"} onChange={SignUpFormOnChg} required/>
+                <input type={"text"} name={"nickname"} onChange={handleInputChg} required/>
                 <br/>
-                <DuplicationBtn setIsDuplicationOk={setIsDuplicationOk} targetValue={signUpFormState.nickname}
+                <DuplicationBtn setIsDuplicationOk={setIsDuplicationOk} targetValue={inputFormState.nickname}
                                 type={"nickname"}/> {/*닉네임 중복확인*/}
                 <br/>
                 <p>이메일</p>
-                <input type={"email"} name={"email"} onChange={SignUpFormOnChg} required/>
+                <input type={"email"} name={"email"} onChange={handleInputChg} required/>
                 <br/>
-                <DuplicationBtn setIsDuplicationOk={setIsDuplicationOk} targetValue={signUpFormState.email}
+                <DuplicationBtn setIsDuplicationOk={setIsDuplicationOk} targetValue={inputFormState.email}
                                 type={"email"}/> {/*이메일 중복확인*/}
                 <p>비밀번호</p>
-                <input type={"password"} name={"password"} onChange={SignUpFormOnChg} required
+                <input type={"password"} name={"password"} onChange={handleInputChg} required
                        placeholder={"8자이상 소문자 대문자 특수문자 포함"}/>
                 <br/>
                 <p>비밀번호 확인</p>
