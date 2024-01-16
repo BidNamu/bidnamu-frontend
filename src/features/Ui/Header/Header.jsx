b4f1762bbafacda3623a8e4ce9fcfba5db3b7439
sanghyeonLee2 <hyeon012366@gmail.com>
2024-01-16 16:25:55 +0900

    feat: 로그인 성공 여부에 따른 마운트되는 레이아웃 분리

--- src/features/Ui/Header/Header.jsx
+++ src/features/Ui/Header/Header.jsx
@@ -1,21 +1,28 @@
 import React, {useState} from 'react';
 import {LogoImg, UserAuthDiv} from "./HeaderLayout";
-import LogInModal from "../Modal/LogInModal";
-import {useAtom} from "jotai";
+import {useAtom, useAtomValue} from "jotai";
 import {signUpPopupAtom} from "../../../store/flagState"
 import {Link} from "react-router-dom";
+import LogInModal from "../../Users/LogIn/LogInModal";
+import {logInAtom} from "../../../store/userInfo";
 
 function Header(props) {
     const [isModalOpen, setIsModalOpen] = useAtom(signUpPopupAtom)
+    const logInState = useAtomValue(logInAtom)
     return (
         <>
             <Link to={"/"}>
                 <LogoImg src={require("../../../assets/img/logo1.jpg")} width={80} height={80}
                          alt={"React"}/></Link>
-            <UserAuthDiv>
-                <Link to={"/sign-up"}><h3>회원가입</h3></Link>
-                <h3 onClick={() => setIsModalOpen(true)}>로그인</h3>
-            </UserAuthDiv>
+            {logInState.isLogIn ?
+                <UserAuthDiv>
+                    <h3>{logInState.nickname}님 반갑습니다.</h3>
+                </UserAuthDiv>
+                :
+                <UserAuthDiv>
+                    <Link to={"/sign-up"}><h3>회원가입</h3></Link>
+                    <h3 onClick={() => setIsModalOpen(true)}>로그인</h3>
+                </UserAuthDiv>}
             {isModalOpen && <LogInModal setIsModalOpen={setIsModalOpen}/>}
         </>
     );
