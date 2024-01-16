18adb30ef6e12b8f04a31435412fa31b9978c422
sanghyeonLee2 <hyeon012366@gmail.com>
2024-01-16 16:29:17 +0900

    feat: 로그인 요청, 에러처리, 로그인 후 토큰 저장, 로그인 성공 여부 전역 상태 저장 로직 작성

--- src/features/Users/LogIn/LogInModal.jsx
+++ src/features/Users/LogIn/LogInModal.jsx
@@ -1,18 +1,49 @@
 import React from 'react';
 import {LogInLayout} from "./ModelLayout";
-
-
+import {useInput} from "../../../hooks/useInput";
+import {authInstance} from "../../../apis/utils/instance";
+import {useNavigate} from "react-router-dom";
+import {useAtom} from "jotai";
+import {isLogInSuccessAtom} from "../../../store/flagState";
 
 function LogInModal({setIsModalOpen}) {
+    const navigate = useNavigate()
+    const [isLogInSuccess,setIsLogInSuccess] = useAtom(isLogInSuccessAtom)
+    const [handleInputChg, inputFormState] = useInput({
+        email: "",
+        password: ""
+    })
+    console.log(inputFormState)
+    const logInFormOnSubmit = async (e) => {
+        e.preventDefault()
+        const result = await authInstance.post("/auths/login", inputFormState)
+        try {
+            console.log(result)
+            if (result.status === 200) {
+                console.log(result.data)
+                localStorage.setItem("accessToken", result.data.accessToken)
+                localStorage.setItem("refreshToken", result.data.refreshToken)
+                setIsLogInSuccess(!isLogInSuccess)
+                alert("로그인을 완료하였습니다.")
+                navigate("/")
+            }
+        } catch (err) {
+            console.log("실패")
+            alert("서버 통신 실패")
+        }
+    }
+
     return (
         <LogInLayout>
-            <form>
-                <span onClick={()=>{setIsModalOpen(false)}}>취소</span>
+            <form onSubmit={logInFormOnSubmit}>
+                <span onClick={() => {
+                    setIsModalOpen(false)
+                }}>취소</span>
                 <p>이메일</p>
-                <input type={"text"}/>
+                <input type={"text"} name={"email"} onChange={handleInputChg}/>
                 <br/>
                 <p>비밀번호</p>
-                <input type={"text"}/>
+                <input type={"password"} name={"password"} onChange={handleInputChg}/>
                 <br/>
                 <button type={"submit"}>로그인</button>
             </form>
