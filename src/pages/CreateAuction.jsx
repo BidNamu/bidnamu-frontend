ffd20b8dd8f8099681182d60335869d3fefe6446
sanghyeonLee2 <hyeon012366@gmail.com>
2024-01-17 15:17:53 +0900

    feat: 경매 등록 페이지 생성

---
+++ src/pages/CreateAuction.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CreateAuctionForm from "../features/Auction/CreateAuction/CreateAuctionForm";
+
+function CreateAuction(props) {
+    return (
+        <>
+            <CreateAuctionForm/>
+        </>
+    );
+}
+
+export default CreateAuction;
