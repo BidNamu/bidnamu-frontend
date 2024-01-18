02e4a03fcac7d9c51cad52acb1ae1d22ff0b129a
sanghyeonLee2 <hyeon012366@gmail.com>
2024-01-18 20:00:27 +0900

    feat: 파일 데이터 가공 로직의 재사용과 단순화를 위한 useFile 커스텀 훅 기능 추가

--- src/hooks/useFile.js
+++ src/hooks/useFile.js
@@ -2,13 +2,11 @@ import React, {useState} from 'react';
 
 export function useFile(initialValue) {
     const [imgList, setImgList] = useState(initialValue)
-    const formData = new FormData();
     const handleImgChg = (e) => {
-        setImgList([...imgList, ...e.target.files]);
-        imgList.forEach(image => {
-            formData.append('files', image);
-        });
+        setImgList([...e.target.files]);
+        console.log(imgList)
     }
+
     return [handleImgChg, imgList]
 }
 
