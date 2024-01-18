032cbb5ebf066f0ae1b9cf51f3a966407aad7f32
sanghyeonLee2 <hyeon012366@gmail.com>
2024-01-18 19:56:23 +0900

    feat: multipart/form-data 전송을 위한 인스턴스 추가

--- src/apis/utils/instance.js
+++ src/apis/utils/instance.js
@@ -16,29 +16,44 @@ export const instance = axios.create({
     //timeout:1000,
     headers: {
         "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("accessToken")}`
     }
 })
-// timestamp : 1970년 1월 1일 00:00:00 UTC(협정 세계시)를 시작점으로 하여 그 시점부터의 경과 시간을 초 단위로 나타내는 시간 코드
-instance.interceptors.request.use(
-    async (config) => {
-        const accessToken = localStorage.getItem("accessToken")
-        const refreshToken = localStorage.getItem("refreshToken")
-        if (!expValidation(accessToken) && expValidation(refreshToken)) {
-            console.log(config)
-            await fetchReissue(refreshToken, accessToken)
-            return config
-        }
-        if (!expValidation(refreshToken)) {
-            localStorage.removeItem("accessToken")
-            localStorage.removeItem("refreshToken")
-            alert("토큰이 만료되었습니다. 로그인을 다시 해주세요.")
-            return config
-        }
-        return config
-    }, function (err) {
-        return Promise.reject(err)
+export const multipartInstance = axios.create({
+    baseURL: API_KEY,
+    //timeout:1000,
+    headers: {
+        'Content-Type': 'multipart/form-data',
+        Authorization: `Bearer ${localStorage.getItem("accessToken")}`
     }
-)
+})
+
+// timestamp : 1970년 1월 1일 00:00:00 UTC(협정 세계시)를 시작점으로 하여 그 시점부터의 경과 시간을 초 단위로 나타내는 시간 코드
+const setupInterceptor = (instance) => {
+    instance.interceptors.request.use(
+        async (config) => {
+            const accessToken = localStorage.getItem("accessToken")
+            const refreshToken = localStorage.getItem("refreshToken")
+            if (!expValidation(accessToken) && expValidation(refreshToken)) {
+                console.log(config)
+                await fetchReissue(refreshToken, accessToken)
+            }
+            if (!expValidation(refreshToken)) {
+                localStorage.removeItem("accessToken")
+                localStorage.removeItem("refreshToken")
+                alert("토큰이 만료되었습니다. 로그인을 다시 해주세요.")
+            }
+            return config
+        }, function (err) {
+            return Promise.reject(err)
+        }
+    )
+}
+
+// 각 인스턴스에 대해 인터셉터 설정
+setupInterceptor(instance);
+setupInterceptor(multipartInstance);
+
 const expValidation = (token) => {
     const currentTimestamp = Math.floor(Date.now() / 1000);
     const {exp} = JSON.parse(base64.decode(token.substring(token.indexOf('.') + 1, token.lastIndexOf('.'))));
@@ -50,10 +65,6 @@ const fetchReissue = async (refreshToken, accessToken) => {
     try {
         const result = await instance.post("/auth/reissue", {
             refreshToken
-        }, {
-            headers: {
-                Authorization: `Bearer ${accessToken}`
-            }
         })
         if (result.status === 200) {
             localStorage.setItem("accessToken", result.data.accessToken)
