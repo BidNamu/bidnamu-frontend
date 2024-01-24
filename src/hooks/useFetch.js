f379509b0d819f4df709587b974407fcfb6b494b
sanghyeonLee2 <hyeon012366@gmail.com>
2024-01-24 23:38:15 +0900

    feat: 입력폼 상태를 post 요청하는 커스텀 훅 로직 작성

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,11 +1,10 @@
-import {multipartInstance} from "../apis/utils/instance";
+import {authInstance, instance, multipartInstance} from "../apis/utils/instance";
 
 
 function usePostWithFiles(inputFormState, imgList, url) {
-    const submitForFetch = async (e) => {
+    const submitWithFiles = async (e) => {
         e.preventDefault()
         const formData = new FormData()
-
         imgList.forEach(img => {
             formData.append('images', img)
         })
@@ -25,8 +24,29 @@ function usePostWithFiles(inputFormState, imgList, url) {
         }
     }
 
-    return [submitForFetch]
+    return [submitWithFiles]
 }
 
-export {usePostWithFiles}
+function usePost(inputFormState, url) {
+
+    const submitForPost = async () => {
+        console.log(inputFormState)
+        try {
+            const result = await authInstance.post(url, inputFormState);
+            //회원가입:authInstance
+            console.log(result)
+            if (result.status === 200) {
+                alert("회원가입을 완료하였습니다.")
+            }
+        } catch (err) {
+            console.log(err)
+            alert("서버 통신 실패")
+        }
+    }
+
+    return [submitForPost]
+}
+
+
+export {usePostWithFiles, usePost}
 
