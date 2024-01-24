8be142a38174bd0506a70fef03b52963ca5778a7
sanghyeonLee2 <hyeon012366@gmail.com>
2024-01-24 23:36:31 +0900

    refactor: react-hook-form 라이브러리 적용으로 기존 상태 useState -> useForm 변경, 그에따른 로직 수정

--- src/features/Users/SignUp/SignUpForm.jsx
+++ src/features/Users/SignUp/SignUpForm.jsx
@@ -1,80 +1,116 @@
-import React, {useRef, useState} from 'react';
+import React from 'react';
 import {SignUpFormLayout} from "./SignUpFormLayout";
-import DuplicationBtn from "./DuplicationBtn";
 import {useNavigate} from "react-router-dom";
 import {useInput} from "../../../hooks/useInput";
+import {usePost} from "../../../hooks/useFetch";
+import {useForm} from "react-hook-form";
+import {DevTool} from "@hookform/devtools";
 import {authInstance} from "../../../apis/utils/instance";
 
+const REGEX = {
+    nickname: /^[^\s]*$/,
+    email: /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/,
+    password: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[#$^+=!*()@%&]).*$/
+}
+
 function SignUpForm(props) { //닉네임(중복확인), 이메일(중복확인), 비밀번호
-    const navigate = useNavigate()
-    const [handleInputChg, inputFormState] = useInput({
-        nickname: "",
-        email: "",
-        password: ""
+    const {
+        control, register, trigger,
+        getValues,
+        formState: {errors}, handleSubmit
+    } = useForm({
+        defaultValues: {
+            nickname: "",
+            email: "",
+            password: ""
+        }
+        //mode 의 디폴트는 onSubmit
     })
-    const [isDuplicationOk, setIsDuplicationOk] = useState(
-        {
-            nicknameDuplicationOk: false,
-            emailDuplicationOk: false
+    const [submitForPost] = usePost({
+        nickname: getValues().nickname,
+        email: getValues().email,
+        password: getValues().password
+    }, "/users")
+
+    const nicknameRegister = register(
+        "nickname", {
+            required: "닉네임을 입력해주세요.",
+            pattern: {value: REGEX.nickname, message: "공백은 허용하지 않습니다."},
+            minLength: {value: 5, message: "5자 이상 입력해주세요."},
+            maxLength: {value: 15, message: "15자 이하 입력해주세요."},
+            validate: {
+                nicknameCheck: async (fieldValue) => {
+                    const result = await authInstance.get(`users/nickname/duplicated/${fieldValue}`)
+                    return result.status === 200 && console.log(result);
+                }
+            }
         }
     )
-    const checkPwRef = useRef(null)
-    const formOnSubmit = async (e) => {
-        e.preventDefault()
-        if (!isDuplicationOk.emailDuplicationOk) {
-            return alert("이메일 중복확인을 하세요")
-        }
-        if (!isDuplicationOk.nicknameDuplicationOk) {
-            return alert("닉네임 중복확인을 하세요")
-        }
-        const result = await authInstance.post("/users", inputFormState)
-        try {
-            console.log(result)
-            if (result.status === 201) {
-                alert("회원가입을 완료하였습니다.")
-                return navigate("/")
+    const emailRegister = register(
+        "email", {
+            required: "이메일을 입력해주세요.",
+            pattern: {value: REGEX.email, message: "올바른 이메일 형식이 아닙니다."},
+            minLength: {value: 6, message: "6자 이상 입력해주세요."},
+            maxLength: {value: 320, message: "320자 이하 입력해주세요."},
+            validate: {
+                emailCheck: async (fieldValue) => {
+                    const result = await authInstance.get(`users/email/duplicated/${fieldValue}`)
+                    console.log(result.status)
+                    return result.status === 200 && console.log(result);
+                }
             }
-        } catch (err) {
-            console.log("실패")
-            alert("서버 통신 실패")
+        })
+    const passwordRegister = register(
+        "password", {
+            required: "비밀번호를 입력해주세요.",
+            pattern: {value: REGEX.password, message: "8자이상 소문자 대문자 특수문자 포함 해야합니다."},
+            minLength: {value: 8, message: "8자 이상 입력해주세요."},
+            maxLength: {value: 20, message: "20자 이하 입력해주세요."},
+        })
+    const passwordCheckRegister = register(
+        "passwordCheck", {
+            required: "비밀번호를 재입력해주세요.",
+            pattern: {value: REGEX.password, message: "8자이상 소문자 대문자 특수문자 포함 해야합니다."},
+            minLength: {value: 8, message: "8자 이상 입력해주세요."},
+            maxLength: {value: 20, message: "20자 이하 입력해주세요."},
         }
+    )
+    const err = (e) => {
+        console.log(e)
+        console.log(errors)
     }
-
-    const checkPwOnChg = (e) => {
-        if (inputFormState.password === e.target.value) {
-            checkPwRef.current.innerText = "비밀번호가 일치합니다."
-            return
-        }
-        checkPwRef.current.innerText = "비밀번호가 일치하지 않습니다."
-    }
-
-
     return (
         <SignUpFormLayout>
-            <form onSubmit={formOnSubmit}>
+            <form onSubmit={handleSubmit(submitForPost, err)}>
                 <h2>회원가입</h2>
                 <p>닉네임</p>
-                <input type={"text"} name={"nickname"} onChange={handleInputChg} required/>
+                <input
+                    {...nicknameRegister} type={"text"}
+                />
+                <p> {errors.nickname?.message}</p>
                 <br/>
-                <DuplicationBtn setIsDuplicationOk={setIsDuplicationOk} targetValue={inputFormState.nickname}
-                                type={"nickname"}/> {/*닉네임 중복확인*/}
+                <button type={"button"} name={"nickname"} onClick={() => trigger("nickname")}>
+                    중복확인
+                </button>
                 <br/>
                 <p>이메일</p>
-                <input type={"email"} name={"email"} onChange={handleInputChg} required/>
+                <input {...emailRegister} type={"email"}/>
+                <p> {errors.email?.message}</p>
                 <br/>
-                <DuplicationBtn setIsDuplicationOk={setIsDuplicationOk} targetValue={inputFormState.email}
-                                type={"email"}/> {/*이메일 중복확인*/}
+                <button type={"button"} name={"email"} onClick={() => trigger("email")}>중복확인</button>
                 <p>비밀번호</p>
-                <input type={"password"} name={"password"} onChange={handleInputChg} required
+                <input {...passwordRegister}
+                       type={"password"} name={"password"}
                        placeholder={"8자이상 소문자 대문자 특수문자 포함"}/>
+                <p> {errors.password?.message}</p>
                 <br/>
                 <p>비밀번호 확인</p>
-                <input type={"password"} onChange={checkPwOnChg} placeholder={"8자이상 소문자 대문자 특수문자 포함"} required/>
-                <br/>
-                <small ref={checkPwRef}></small>
+                <input {...passwordCheckRegister} type={"password"} placeholder={"8자이상 소문자 대문자 특수문자 포함"}/>
+                <p> {errors.passwordCheck?.message}</p>
                 <br/>
                 <button type={"submit"}>회원가입</button>
             </form>
+            <DevTool control={control}/>
         </SignUpFormLayout>
     );
 }
