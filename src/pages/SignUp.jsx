657175288cef816f72f076d16befed917e3511aa
sanghyeonLee2 <hyeon012366@gmail.com>
2024-01-30 20:15:14 +0900

    style: 페이지에서 SignUpForm.jsx 렌더링 하기 위한 SignUp.jsx 파일 생성

---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SignUpForm from "../features/Users/SignUp/SignUpForm";
+
+function SignUp(props) {
+    return (
+        <>
+            <SignUpForm/>
+        </>
+    );
+}
+
+export default SignUp;
