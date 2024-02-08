d089d22b85c5b6dc82c5e9b479f5fe8ba77db1aa
sanghyeonLee2 <hyeon012366@gmail.com>
2024-02-08 22:34:06 +0900

    refactor: get 요청 로직 재사용을 위한 useGet 커스텀 훅 추가 및 파일 이 포함된 요청 로직 수정

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,17 +1,20 @@
-import {authInstance, multipartInstance} from "../apis/utils/instance";
+import {authInstance, instance, multipartInstance} from "../apis/utils/instance";
 import {useNavigate} from "react-router-dom";
+import {useEffect, useState} from "react";
+import {useAtom} from "jotai";
+import {isLogInSuccessAtom} from "../store/flagState";
 
 
-function usePostWithFiles(inputFormState, imgList, url) {
-    const submitWithFiles = async (e) => {
-        e.preventDefault()
+function usePostWithFiles(url) {
+    return async (inputFormState) => {
+        const {images, ...rest} = inputFormState
         const formData = new FormData()
-        imgList.forEach(img => {
+        Array.from(images).forEach(img => {
             formData.append('images', img)
         })
         formData.append(
             'body',
-            new Blob([JSON.stringify(inputFormState)], {type: "application/json"})
+            new Blob([JSON.stringify(rest)], {type: "application/json"})
         );
         try {
             const result = await multipartInstance.post(url, formData);
@@ -20,34 +23,59 @@ function usePostWithFiles(inputFormState, imgList, url) {
                 alert("회원가입을 완료하였습니다.")
             }
         } catch (err) {
-            console.log("실패")
+            console.dir(err)
             alert("서버 통신 실패")
         }
     }
-
-    return [submitWithFiles]
 }
 
 function usePost(url) {
     const navigate = useNavigate()
+    const [isLogInSuccess, setIsLogInSuccess] = useAtom(isLogInSuccessAtom)
     return async (inputFormState) => {
         console.log(inputFormState)
         try {
-            const result = await authInstance.post(url,inputFormState);
+            const result = await authInstance.post(url, inputFormState);
             //회원가입:authInstance
             console.log(result)
             if (result.status === 201) {
                 alert("회원가입을 완료하였습니다.")
                 navigate("/")
             }
+            if (result.status === 200) {
+                localStorage.setItem("accessToken", result.data.accessToken)
+                localStorage.setItem("refreshToken", result.data.refreshToken)
+                setIsLogInSuccess(!isLogInSuccess)
+                alert("로그인을 완료하였습니다.")
+                navigate("/")
+            }
         } catch (err) {
-            console.log(err)
             alert("서버 통신 실패")
         }
     }
 }
 
+function useGet(url) {
+    const [data, setData] = useState([])
+    useEffect(() => {
+        getData()
+    }, [])
+    const getData = async () => {
+        try {
+            const result = await instance.get(url);
+            //회원가입:authInstance
+            if (result.status === 200) {
+                setData(result.data)
+                console.log(result.data)
+            }
+        } catch (err) {
+            console.log(err)
+            alert("서버 통신 실패")
+        }
+    }
+    return data
+}
 
 
-export {usePostWithFiles, usePost}
+export {usePostWithFiles, usePost, useGet}
 
