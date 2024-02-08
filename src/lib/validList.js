97432997bcf958e6fde5dc8afa16f175680d419c
sanghyeonLee2 <hyeon012366@gmail.com>
2024-02-08 22:36:10 +0900

    feat: 경매 등록 유효성 검사 yup 스키마 추가

--- src/lib/validList.js
+++ src/lib/validList.js
@@ -52,3 +52,23 @@ export const logInValid = yup.object({
             .matches(REGEX.password, "8자이상 소문자 대문자 특수문자 포함 해야합니다."),
     }
 )
+
+export const createAuctionValid = yup.object({
+        title: yup.string()
+            .required("상품명을 입력 해주세요.")
+            .min(2, "2자 이상 입력 해주세요.")
+            .max(10, "10자 이하 입력 해주세요."),
+        description: yup.string()
+            .required("상품 상세정보를 입력 해주세요.")
+            .max(60, "60자 이하 입력 해주세요."),
+        categoryId: yup.number()
+            .required("카테고리를 선택해 주세요."),
+        startingBid: yup.number()
+            .required("경매 시작 가격을 입력 해주세요."),
+        fixedPrice: yup.boolean(),
+        images: yup.mixed()
+            .required("상품 사진을 동록 해주세요."),
+        closingTime: yup.string()
+            .required("종료 날짜를 입력 해주세요.")
+    }
+)
